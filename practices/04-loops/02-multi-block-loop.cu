b77308671d5dbc28b6acb176e2ef9b5894aab56f
Wonjun <wjjgjk@naver.com>
2025-01-01 13:51:03 -0600

    SOLVE: 04-02-multi-block-loop.cu

--- practices/04-loops/02-multi-block-loop.cu
+++ practices/04-loops/02-multi-block-loop.cu
@@ -5,12 +5,9 @@
  * only do the work of 1 iteration of the original loop.
  */
 
-void loop(int N)
+__global__ void loop()
 {
-  for (int i = 0; i < N; ++i)
-  {
-    printf("This is iteration number %d\n", i);
-  }
+  printf("This is iteration number %d\n", threadIdx.x + blockIdx.x * blockDim.x);
 }
 
 int main()
@@ -24,6 +21,6 @@ int main()
    * the execution configuration.
    */
 
-  int N = 10;
-  loop(N);
+  loop<<<2, 1024>>>();
+  cudaDeviceSynchronize();
 }
